b8e899b2524dae7ee6fbc163f7e97797a6b58ca0
Roman Zaynullin <r0mulusss@yandex.ru>
2019-06-24 13:11:11 +0500

    added menu active state, input validation and form handling

--- docs/assets/scripts/app.js
+++ docs/assets/scripts/app.js
@@ -1,26 +1,81 @@
-var func = {
-  
+const func = {
+  activeMenu() {
+    const items = document.querySelectorAll('.aside__item')
+    items.forEach((item) => {
+      item.addEventListener('click', (e) => {
+        if(!e.target.classList.contains('aside__item_active')) {
+          items.forEach((item) => {
+            item.classList.remove('aside__item_active')
+          })
+          e.target.classList.add('aside__item_active')
+        }
+      })
+    })
+  },
+  validationCard() {
+    const cards = document.querySelectorAll('.form__input_card')
+    cards.forEach((card) => {
+      card.addEventListener('blur', (e) => {
+        if (!/\d{4}/.test(e.target.value) || e.target.value < 4) {
+          card.classList.add('form__input_error')
+        } else {
+          card.classList.remove('form__input_error')
+        }
+      })
+    })
+  },
+  validationName() {
+    const name = document.querySelector('.form__input_name')
+    name.addEventListener('blur', (e) => {
+      if (!/([A-Za-z\. -]+)/.test(e.target.value) || e.target.value.length < 4) {
+        name.classList.add('form__input_error')
+      } else {
+        name.classList.remove('form__input_error')
+      }
+    })
+  },
+  validationCVV() {
+    const cvv = document.querySelector('.form__input_cvv')
+    cvv.addEventListener('blur', (e) => {
+      if (!/\d{3}/.test(e.target.value) || e.target.value < 3) {
+        cvv.classList.add('form__input_error')
+      } else {
+        cvv.classList.remove('form__input_error')
+      }
+    })
+  },
+  formSubmit() {
+    const form = document.querySelector('.form')
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      console.log('Form Submitted')
+    })
+  } 
 }
 
-var app = {
-  'init': function () {
-    
+const app = {
+  'init': () => {
+    func.activeMenu()
+    func.validationCard()
+    func.validationName()
+    func.validationCVV()
+    func.formSubmit()
   },
-  'scroll': function () {
+  'scroll': () => {
   },
-  'load': function () {
+  'load': () => {
   },
-  'resize': function () {
+  'resize': () => {
   },
 };
 
 app.init();
-window.addEventListener("load", function () {
+window.addEventListener("load", () => {
   app.load();
 });
-window.addEventListener("resize", function () {
+window.addEventListener("resize", () => {
   app.resize();
 });
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", () => {
   app.scroll();
 })
